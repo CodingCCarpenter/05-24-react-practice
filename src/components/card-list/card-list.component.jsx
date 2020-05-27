dfb20989191dc8c951cb482ad61e5387382adb5c
CodingCCarpenter <christineassists@aol.com>
2020-05-26 21:53:04 -0500

    Add styles.css file to the card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,7 +4,7 @@ import './card-list.styles.css';
 export const CardList = (props) => {
     console.log(props);
     return( 
-        <div>
+        <div className='card-list'>
             { props.children }
         </div>
     )
