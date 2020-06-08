48c96d0afaf9947aa02973984ce68aaf5aba3403
CodingCCarpenter <Christineassists@aol.com>
2020-06-07 23:22:55 -0500

    add onChange event handler to setState to value

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -20,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters'/>
+        <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
         <CardList monsters={this.state.monsters} />
       </div>
     )
