7d203693244c4c99b9a69f3a66bf96f500d31e43
CodingCCarpenter <Christineassists@aol.com>
2020-06-23 17:25:45 -0500

    updating onChange handler to set the state to the value of the searchField and console.log state

--- src/App.js
+++ src/App.js
@@ -21,7 +21,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => console.log(e)}/>
+        <input type='search' placeholder='search monsters' onChange={e => {
+          this.setState({ searchField: e.target.value });
+          console.log(this.state)}
+        }/>
         <CardList monsters={this.state.monsters} />
       </div>
     )
