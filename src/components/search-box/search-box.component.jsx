d369029b017b3d9cfd3f4b49f41669c7f2cfe968
CodingCCarpenter <Christineassists@aol.com>
2020-06-23 18:33:58 -0500

    passing in destructured placholder and handleChange to be passed in dynamically as values of properties on input

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-const SearchBox = () => 
+const SearchBox = ( {placeholder, handleChange} ) => 
 (
     <input 
         className='search'
         type='search' 
-        placeholder='search monsters' 
-        onChange={e => 
-        this.setState({ searchField: e.target.value })
-    }/>
+        placeholder= { placeholder }
+        onChange={ handleChange }/>
 )
 
 export default SearchBox;
