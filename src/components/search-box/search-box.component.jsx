5e3f45094029c15bdfdd715b6b58e40c41c77751
CodingCCarpenter <Christineassists@aol.com>
2020-06-23 18:01:46 -0500

    adding search box logic from app.js to search-box.component.jsx

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-const SearchBox = () => {
-    
-}
+const SearchBox = () => 
+(
+    <input 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={e => 
+        this.setState({ searchField: e.target.value })
+    }/>
+)
+
+export default SearchBox;
